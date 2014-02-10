ac9148c315b93576ee9586858409c60bf7c5e6ef
David Dominguez <ddominguez@gmail.com>
2014-02-09 23:51:42 -0500

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='marvelpy',
+    description='A simple python wrapper for The Marvel Comics API.',
+    author='David Dominguez',
+    version='0.1',
+    packages=['marvelpy'],
+    install_requires=['requests'],
+    license='MIT'
+)
