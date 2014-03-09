47e9af9ddf487dfdb2b53feac2e7046d51bf3903
David Dominguez <ddominguez@gmail.com>
2014-03-08 22:48:21 -0500

    reduce duplicate code
    * added _get_response
    * added _request_uri

--- marvelpy/marvelpy.py
+++ marvelpy/marvelpy.py
@@ -12,12 +12,14 @@ class Marvel(object):
         self._api_key = api_key
         self._private_key = private_key
         self._base_uri = 'http://gateway.marvel.com/v%s/public' % version
+        self._request_uri = None
 
     def get(self, uri, params={}, etag=None):
         if uri.strip() == '':
             raise MarvelpyError('Resource URI is blank.')
         if not uri.startswith(self._base_uri):
             raise MarvelpyError('Invalid Marvel API URI.')
+        self._request_uri = uri
         ts = str(time())
         hash = md5(ts + self._private_key + self._api_key)
         params.update({'apikey': self._api_key, 'ts': ts, 'hash': hash.hexdigest()})
@@ -49,26 +51,24 @@ class Marvel(object):
 
         return (kw['id'], kw['list_type'], kw['params'], kw['etag'])
 
-    def characters(self, **kwargs):
+    def _get_response(self, collection, kwargs):
         id, list_type, params, etag = self._kwargs(kwargs)
-        return self.get(self._resource_uri('characters', id, list_type), params, etag)
+        return self.get(self._resource_uri(collection, id, list_type), params, etag)
+
+    def characters(self, **kwargs):
+        return self._get_response('characters', kwargs)
 
     def comics(self, **kwargs):
-        id, list_type, params, etag = self._kwargs(kwargs)
-        return self.get(self._resource_uri('comics', id, list_type), params, etag)
+        return self._get_response('comics', kwargs)
 
     def creators(self, **kwargs):
-        id, list_type, params, etag = self._kwargs(kwargs)
-        return self.get(self._resource_uri('creators', id, list_type), params, etag)
+        return self._get_response('creators', kwargs)
 
     def events(self, **kwargs):
-        id, list_type, params, etag = self._kwargs(kwargs)
-        return self.get(self._resource_uri('events', id, list_type), params, etag)
+        return self._get_response('events', kwargs)
 
     def series(self, **kwargs):
-        id, list_type, params, etag = self._kwargs(kwargs)
-        return self.get(self._resource_uri('series', id, list_type), params, etag)
+        return self._get_response('series', kwargs)
 
     def stories(self, **kwargs):
-        id, list_type, params, etag = self._kwargs(kwargs)
-        return self.get(self._resource_uri('stories', id, list_type), params, etag)
+        return self._get_response('stories', kwargs)
